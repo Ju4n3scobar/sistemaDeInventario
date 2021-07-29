35bced5e03b0bd5e8b992316dfdf6bfe2fe3609b
Practicante Desarrollo <prdesarrollo@comodisimos.com.co>
2021-07-29 17:05:12 -0500

    Retornar en el metodo show, caracteristicas en clave valor

--- app/Http/Controllers/APIs/logs.php
+++ app/Http/Controllers/APIs/logs.php
@@ -56,26 +56,42 @@ class logs extends Controller
         $logs = ModelsLogs::all();
         $total_records = count($logs);
         $counter = 1;
-        $number_records_characteristics = 1;
         for ($i = 1; $i <= $total_records; $i++) {
-            $current_records = ModelsLogs::where('id', $i)->first();
-            $array_characteristics = explode('=>', $current_records->characteristics);
-            foreach ($array_characteristics as $word) {
-                if ($counter == 1 or $counter % 6 == 0) {
-                    $response_characteristics[$counter] = $number_records_characteristics;
-                    $counter++;
-                    $response_characteristics[$counter] = $word;
-                    $number_records_characteristics++;
-                } else {
-                    $response_characteristics[$counter] = $word;
+            $current_records = ModelsLogs::where('id', $i)->get();
+            if(isset($current_records->characteristics)){
+                $array_characteristics = explode('=>', $current_records->characteristics);
+                foreach ($array_characteristics as $word) {
+                    
+                    $key_value_characteristics = explode('=>', $current_records['characteristics']);
+
+                            $characteristics_response[$key_value_characteristics[0]] = $key_value_characteristics[1];
+                            $characteristics_response[$key_value_characteristics[2]] = $key_value_characteristics[3];
+                            $counter++; 
+                            
+                            // $characteristics_response[$counter]=$key_value_characteristics[1];
+                                
+                           
+                            // $characteristics_response[$counter]=$key_value_characteristics[1];
+                    
+                    
+                    // if ($counter == 1 or $counter % 6 == 0) {
+                    //     $response_characteristics[$counter] = $number_records_characteristics;
+                    //     $counter++;
+                    //     $response_characteristics[$counter] = $word;
+                    //     $number_records_characteristics++;
+                    // } else {
+                    //     $response_characteristics[$counter] = $word;
+                    // }
+                    // $counter++;
                 }
-                $counter++;
+                
             }
+            
         }
 
         return response()->json([
             $logs,
-            'Todas las caracteristicas, segun el id al registro que pertenecen, listas para ser mostradas' => $response_characteristics
+            'Todas las caracteristicas, segun el id al registro que pertenecen, listas para ser mostradas' => $characteristics_response
         ]);
     }
 }
